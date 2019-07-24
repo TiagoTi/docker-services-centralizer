1632e0f129981d5ca7cc16b92e6613946de9f91c
TiagoTi <tiago@ti2.net.br>
2019-07-24 20:35:56 -0300

    update

--- container.py
+++ container.py
@@ -2,12 +2,23 @@
 import sys
 import os
 import argparse
+import glob
 
+NAME="services"
+PATH="/mnt/hd_um_tera/tiago/services"
+PROJECT_DIRECTORY = f" --project-directory {PATH} "
+PROJECT_NAME = f" --project-name {NAME} "
+DOCKER_COMPOSE = f"docker-compose {PROJECT_DIRECTORY} {PROJECT_NAME} --file "
 
 def run_linux_command(command):
     print(command)
     os.system(command)
 
+def grouping_files():
+    files_ = []
+    files = [f for f in glob.glob(PATH + "**/*.yaml", recursive=True)]
+    files = " --file ".join(files)
+    return files
 
 def docker_compose():
     parser = argparse.ArgumentParser()
@@ -15,12 +26,25 @@ def docker_compose():
     parser.add_argument('commons_args', nargs='*')
     args = parser.parse_args()
 
-
     if args.command:
+        command = ""
+        if args.command == "start":
+            print("starting services")
+            command = f'{DOCKER_COMPOSE} {grouping_files()} start {" ".join(args.commons_args)}'
+            
+        if args.command == "up":
+            print("up services")
+            command = f'{DOCKER_COMPOSE} {grouping_files()} up -d {" ".join(args.commons_args)}'
+
+        if args.command == "stop":
+            print("stops services")
+            command = f'{DOCKER_COMPOSE} {grouping_files()} stop {" ".join(args.commons_args)}'
+
         if args.command == "newlan":
             print("ok vamos criar uma lan")
             command = f"docker network create --driver=bridge --subnet={args.commons_args[0]} {args.commons_args[1]}"
-            run_linux_command(command)
+            
+        run_linux_command(command)
 
 
 if __name__ == '__main__':
